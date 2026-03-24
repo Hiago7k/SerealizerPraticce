405ae4789d3a090cee1dcb5ae1ced3a18c195334
Hiago 7k <hiago7k@gmail.com>
2026-03-24 18:11:54 -0300

    criando a classe do usuar io

---
+++ Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerealizerPraticcel;
+
+internal class Usuario
+{
+    public Usuario(string nomeDoUsuario)
+    {
+        NomeDoUsuario = nomeDoUsuario;
+    }
+
+    public string NomeDoUsuario { get;  }
+}

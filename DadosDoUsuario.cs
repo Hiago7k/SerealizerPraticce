cac8504141aafd05bb0022bef1d3303c8bffa9d5
Hiago 7k <hiago7k@gmail.com>
2026-03-24 18:10:13 -0300

    criando a classe molde, para serealizar os ados

---
+++ DadosDoUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerealizerPraticce;
+
+internal class DadosDoUsuario
+{
+    public string Nome { get;  }
+    public int Idade { get;  }
+    public string Email { get;  }
+}
